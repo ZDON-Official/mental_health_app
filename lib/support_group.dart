868f929e281b7bf0f19d699aa2e5406d04b1e40e
Zohaib <zrana1214@gmail.com>
2022-11-19 16:28:39 -0500

    working on support group page

--- lib/support_group.dart
+++ lib/support_group.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
 
 class SupportGroupPage extends StatefulWidget{
-  
+  const SupportGroupPage({super.key, required this.title})// what does this do?
+
+
+  widget build(BuildContext context){
+    return Scaffold(
+      appBar:AppBar(
+        title: Text(widget.title),
+      ),
+      
+    )
+  }
+
+
 }
